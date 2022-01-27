66a78efe93782c3caa2f2e8b07ed17571d7b8b3d
mario <mario.mazzarelli@asfweb.net>
2022-01-27 16:33:29 +0100

    create library

--- lib/core/models/user.dart
+++
@@ -1,5 +0,0 @@
-class User {
-  final String name;
-  final String surname;
-  User({required this.name, required this.surname});
-}
---
+++ login_smartpa/lib/login_smartpa.dart
@@ -0,0 +1,7 @@
+library login_smartpa;
+
+class User {
+  final String name;
+  final String surname;
+  User({required this.name, required this.surname});
+}
