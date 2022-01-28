384d38c5dbbcac26e02247bada00294344ae4c46
mario <mario.mazzarelli@asfweb.net>
2022-01-28 11:09:44 +0100

    ...

--- lib/login_smartpa.dart
+++ lib/login_smartpa.dart
@@ -1,1 +1,15 @@
 library login_smartpa;
+
+class Login {
+  final String id;
+  final String name;
+  final String surname;
+  final String age;
+
+  Login(
+    this.id,
+    this.name,
+    this.surname,
+    this.age,
+  );
+}
